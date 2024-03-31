d0b2e04bbc02969abf453cd0d99e1de412695476
Abubakar Abubakar Yusif <abubakaryusuf951@gmail.com>
2024-03-31 17:54:43 +0800

    api: add h2ogpt chat routes
    Signed-off-by: Abubakar Abubakar Yusif <abubakaryusuf951@gmail.com>

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
 from app.api.routes import items, login, users, utils
+from app.h2ogpt.src.h2ogpt.api.routes.chats import router
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 api_router.include_router(items.router, prefix="/items", tags=["items"])
+api_router.include_router(router, prefix="/chats", tags=["chats"])
